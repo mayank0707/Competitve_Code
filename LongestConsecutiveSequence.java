78e19613612d477d149c39223e52e4e2a6556e1d
Mayank Gupta <43218169+mayank0707@users.noreply.github.com>
2024-03-11 23:52:56 +0530

    Leetcode problem

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,30 @@
+//Leetcode problem- https://leetcode.com/problems/longest-consecutive-sequence/
+
+import java.util.*;
+
+public class LongestConsecutiveSequence {
+	
+	public int longestConsecutive(int[] nums) {
+        int maxSeq = 0;
+        Set<Integer> sequences = new HashSet<>();
+        for (int num : nums) {
+            sequences.add(num);
+        }
+        for (int num : nums) {
+            int nextInSeq = num + 1;
+            int prevInSeq = num - 1;
+            int currentSequence = 1;
+            while (sequences.remove(prevInSeq--)) {
+                currentSequence++;
+            }
+            while (sequences.remove(nextInSeq++)) {
+                currentSequence++;
+            }
+            if (currentSequence > maxSeq) {
+                maxSeq = currentSequence;
+            }
+        }
+        return maxSeq;
+    }
+	
+}
