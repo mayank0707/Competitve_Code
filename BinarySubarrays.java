1e4e655160cb2089530ed0cf99593c04f8a812cd
Mayank Gupta <43218169+mayank0707@users.noreply.github.com>
2024-03-14 23:52:23 +0530

    Leetcode problem
    Given - Binary array nums and an integer goal.
    Return - number of non-empty subarrays with a sum goal.

---
+++ BinarySubarrays.java
@@ -0,0 +1,22 @@
+//Leetcode problem- https://leetcode.com/problems/binary-subarrays-with-sum/
+
+public class BinarySubarrays {
+	
+	public int numSubarraysWithSum(int[] nums, int goal) {
+        int totalCount = 0;
+        int sum = 0;
+        int prefixSum[] = new int[nums.length + 1];
+        prefixSum[0] = 1;
+        
+        for (int num : nums) {
+            sum += num;
+            if (sum >= goal) {
+                totalCount += prefixSum[sum - goal];
+            }
+            prefixSum[sum]++;
+        }
+        
+        return totalCount;
+    }
+
+}
